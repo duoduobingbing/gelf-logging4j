8d4f2d75acaaa6fbea3ee22480afabe7fec3ffd4
TKtiki <44247218+TKtiki@users.noreply.github.com>
2025-09-05 21:40:32 +0200

    go back to redis:2.8

--- src/test/java/biz/paluch/logging/gelf/RedisSentinelIntegrationTestBase.java
+++ src/test/java/biz/paluch/logging/gelf/RedisSentinelIntegrationTestBase.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.Network;
+import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.MountableFile;
 
 import java.io.IOException;
@@ -13,6 +14,7 @@ import java.nio.file.Path;
 import java.time.Duration;
 import java.util.List;
 
+@Testcontainers
 public class RedisSentinelIntegrationTestBase extends RedisIntegrationTestBase {
 
     public static GenericContainer<?> redisLocalSentinel;
@@ -28,9 +30,9 @@ public class RedisSentinelIntegrationTestBase extends RedisIntegrationTestBase {
 
     @BeforeAll
     static void beforeAll() {
-        RedisIntegrationTestBase.createRedisMasterTestcontainer();
+//        RedisIntegrationTestBase.createRedisMasterTestcontainer();
         redisLocalMasterTestcontainer.withNetwork(network).withNetworkAliases(redisLocalMasterAlias);
-        RedisIntegrationTestBase.startRedisMasterTestcontainer();
+//        RedisIntegrationTestBase.startRedisMasterTestcontainer();
 
         final String sentinelConf =
                 """
@@ -53,7 +55,7 @@ public class RedisSentinelIntegrationTestBase extends RedisIntegrationTestBase {
         }
 
 
-        redisLocalSentinel = new GenericContainer<>("redis:8.2")
+        redisLocalSentinel = new GenericContainer<>("redis:2.8")
                 .withExposedPorts(redisLocalSentinelPort)
                 .withNetwork(network).withNetworkAliases(redisLocalSentinelAlias)
                 .withCommand("redis-sentinel", "/etc/sentinel.conf")
